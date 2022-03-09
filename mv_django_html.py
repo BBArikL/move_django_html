6b4c45712784ea3835b9688dd22ef9acdadc94dc
Arik <BBArik@protonmail.com>
2022-03-09 16:37:55 -0500

    Fix IndentationError

--- mv_django_html.py
+++ mv_django_html.py
@@ -10,9 +10,11 @@ parser = argparse.ArgumentParser(description='Move Html files to and from a djan
 parser.add_argument('--project_path', type=str, help='The path to the Django project. REQUIRED', required=True)
 parser.add_argument('--project_name', type=str,
                     help='The name of the django project. REQUIRED', required=True)
-parser.add_argument('--operation', type=str, help='The operation to do. Supported: move_to, move_from. REQUIRED', required=True)
+parser.add_argument('--operation', type=str, help='The operation to do. Supported: move_to, move_from. REQUIRED',
+                    required=True)
 parser.add_argument('--local', type=str, help='Name of the local directory containing html files. OPTIONAL')
-parser.add_argument('--no_confirm', nargs="?", dest="NO_CONFIRM", default=False, const=True, help='If skip the confirm prompt.')
+parser.add_argument('--no_confirm', nargs="?", dest="NO_CONFIRM", default=False, const=True,
+                    help='If skip the confirm prompt.')
 
 args = parser.parse_args()
 
@@ -44,10 +46,10 @@ if not os.path.exists(local_path):
 
 # Operations, finally!
 if not args.NO_CONFIRM:
-  choice = input("No path errors could be found. The next steps will overwrite all files in the destination "
-               "directories. Double-check your operation before continuing. Do you want to continue? (y/n) ")
- else:
-  choice = "y"
+    choice = input("No path errors could be found. The next steps will overwrite all files in the destination "
+                   "directories. Double-check your operation before continuing. Do you want to continue? (y/n) ")
+else:
+    choice = "y"
 
 if choice[0].lower() == "y":
     operation = args.operation
